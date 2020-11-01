88aed2af31ad1ec203fdc3f624a01d022ca3df9f
caiseyann <caiseyann4@gmail.com>
2020-11-02 00:05:49 +0300

    email authentication added into views.py plus other files, created to require users to confirm before creating an account

--- app/views.py
+++ app/views.py
@@ -13,8 +13,34 @@ from django.shortcuts import render, redirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def signup(request):    
+    if request.method == 'POST':        
+        form = SignupForm(request.POST)        
+        if form.is_valid():            
+            user = form.save(commit=False)            
+            user.is_active = False            
+            user.save()            
+            current_site = get_current_site(request)            
+            mail_subject = 'Activate your account.'            
+            message = render_to_string('active.html', {                
+                'user': user,                
+                'domain': current_site.domain,                
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),                
+                'token':account_activation_token.make_token(user),            })            
+            to_email = form.cleaned_data.get('email')            
+            email = EmailMessage(                        
+            mail_subject, message, to=[to_email]            )            
+            email.send()            
+            return HttpResponse('Please confirm your email address to complete the registration')    
+        else:        
+            form = SignupForm()    
+            return render(request, 'registration_form.html', {'form': form})
+
+
 @login_required(login_url='/accounts/login/')
 def homepage(request):
     if request.user.is_authenticated:
