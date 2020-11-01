53ffce8472c180fa4c21bd3b26db38130942b865
caiseyann <caiseyann4@gmail.com>
2020-11-01 22:40:21 +0300

    Formulated Neighborhood class in models.py

--- app/models.py
+++ app/models.py
@@ -38,3 +38,43 @@ class Profile(models.Model):
         ordering = ['user']
 
 
+class Neighborhood(models.Model):
+    locality = models.CharField(
+        max_length=30, default="e.g Nairobi, Mombassa, Naivasha etc")
+    name = models.CharField(max_length=30)
+    occupants_count = models.IntegerField(default=0, blank=True)
+    # profile=models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    user_profile = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='strip', blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    
+
+    @classmethod
+    def search_neighborhood_by_name(cls, search_term):
+        neighborhoods = cls.objects.filter(name__icontains=search_term)
+        return neighborhoods
+
+    @classmethod
+    def one_neighborhood(cls, id):
+        neighborhood = Neighborhood.objects.filter(id=id)
+        return neighborhood
+
+    @classmethod
+    def all_neighborhoods(cls):
+        neighborhoods = cls.objects.all()
+        return neighborhoods
+
+    @classmethod
+    def get_neighborhood_by_id(cls, id):
+        neighborhood = Neighborhood.objects.filter(id=Neighborhood.id)
+        return neighborhood
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+
+
+
+
