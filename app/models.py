3d98ab09c4b647809c7a688bd256ec2a7d8eb532
caiseyann <caiseyann4@gmail.com>
2020-11-01 22:38:58 +0300

    Formulated profile class in models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='images/', blank=True)
+    contact = HTMLField()
+    email = models.EmailField(max_length=80, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    objects = models.Manager()
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls, id):
+        profile = Profile.objects.get(user=id)
+        return profile
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def find_profile(cls, search_term):
+        profile = Profile.objects.filter(user__username__icontains=search_term)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+    class Meta:
+        ordering = ['user']
+
+
