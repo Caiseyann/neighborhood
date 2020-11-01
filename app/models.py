8afcfb87298af5d23b55ca8a1a0ceba4eea66939
caiseyann <caiseyann4@gmail.com>
2020-11-01 22:41:39 +0300

    Post and join class rendered in models.py

--- app/models.py
+++ app/models.py
@@ -77,4 +77,74 @@ class Neighborhood(models.Model):
 
 
 
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    description = HTMLField(blank=True)
+    email = models.EmailField(max_length=70, blank=True)
+    business_owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    business_strip = models.ForeignKey(
+        Neighborhood, on_delete=models.CASCADE, related_name='business', null=True)
+    objects = models.Manager()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        businesses = cls.objects.filter(name__icontains=search_term)
+        return businesses
+
+    @classmethod
+    def get_neighborhood_businesses(cls, neighborhood_id):
+        businesses = Business.objects.filter(neighborhood_id=id)
+        return businesses
+
+    @classmethod
+    def get_strip_business(cls, business_strip):
+        businesses = Business.objects.filter(business_strip_pk=business_strip)
+        return businesses
+
+    @classmethod
+    def get_profile_businesses(cls, profile):
+        businesses = Business.objects.filter(business_owner__pk=profile)
+        return businesses
+
+
+class Join(models.Model):
+    user_id = models.OneToOneField(User)
+    strip_id = models.ForeignKey(Neighborhood)
+
+    def __str__(self):
+        return self.user_id
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = HTMLField(blank=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post_strip = models.ForeignKey(
+        Neighborhood, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+    @classmethod
+    def search_post(cls, search_term):
+        posts = cls.objects.filter(name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def get_strip_posts(cls, post_strip):
+        posts = Post.objects.filter(post_strip=id)
+        return posts
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        posts = cls.objects.filter(name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def all_posts(cls,id):
+        posts = Post.objects.all()
+        return posts
+
+
+
 
